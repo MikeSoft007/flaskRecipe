5cbae02e854ccbb7156870e896cf15604a5a8a8c
MikeSoft007 <mekpenyong2@gmail.com>
2020-02-29 17:47:12 +0100

    initial commit

---
+++ project/__init__.py
@@ -0,0 +1,60 @@
+from flask import Flask, render_template
+from flask_mysqldb import MySQL
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
+from flask_mail import Mail
+
+
+
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_pyfile('flask.cfg')
+
+
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+mail = Mail(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "users.login"
+
+
+from project.models import User
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.filter(User.id == int(user_id)).first()
+
+####################
+#### blueprints ####
+####################
+
+from project.users.views import users_blueprint
+from project.recipes.views import recipes_blueprint
+
+# register the blueprints
+app.register_blueprint(users_blueprint)
+app.register_blueprint(recipes_blueprint)
+
+############################
+#### custom error pages ####
+############################
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(403)
+def page_not_found(e):
+    return render_template('403.html'), 403
+
+
+@app.errorhandler(410)
+def page_not_found(e):
+    return render_template('410.html'), 410
+
+@app.errorhandler(405)
+def page_not_found(e):
+    return render_template('405.html'), 405
---
+++ project/models.py
@@ -0,0 +1,83 @@
+from project import db, bcrypt
+from sqlalchemy.ext.hybrid import hybrid_method, hybrid_property
+from datetime import datetime
+
+
+class Recipe(db.Model):
+    __tablename__ = "recipes"
+
+    id = db.Column(db.Integer, primary_key=True)
+    recipe_title = db.Column(db.String(255), nullable=False)
+    recipe_description = db.Column(db.String(255), nullable=False)
+
+    def __init__(self, title, description):
+        self.recipe_title = title
+        self.recipe_description = description
+
+    def __repr__(self):
+        return '<title {}'.format(self.name)
+
+
+
+class User(db.Model):
+
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    email = db.Column(db.String(40), unique=True, nullable=False)
+    _password = db.Column(db.Binary(60), nullable=False)
+    authenticated = db.Column(db.Boolean, default=False)
+    email_confirmation_sent_on = db.Column(db.DateTime, nullable=True)
+    email_confirmed = db.Column(db.Boolean, nullable=True, default=False)
+    email_confirmed_on = db.Column(db.DateTime, nullable=True)
+    registered_on = db.Column(db.DateTime, nullable=True)
+    last_logged_in = db.Column(db.DateTime, nullable=True)
+    current_logged_in = db.Column(db.DateTime, nullable=True)
+    role = db.Column(db.String(10), default='user')
+
+    def __init__(self, email, plaintext_password, email_confirmation_sent_on=None, role='user'):
+        self.email = email
+        self.password = plaintext_password
+        self.authenticated = False
+        self.email_confirmation_sent_on = email_confirmation_sent_on
+        self.email_confirmed = False
+        self.email_confirmed_on = None
+        self.registered_on = datetime.now()
+        self.last_logged_in = None
+        self.current_logged_in = datetime.now()
+        self.role = role
+
+    @hybrid_property
+    def password(self):
+        return self._password
+
+    @password.setter
+    def password(self, plaintext_password):
+        self._password = bcrypt.generate_password_hash(plaintext_password)
+
+    @hybrid_method
+    def is_correct_password(self, plaintext_password):
+        return bcrypt.check_password_hash(self.password, plaintext_password)
+
+    @property
+    def is_authenticated(self):
+        """Return True if the user is authenticated."""
+        return self.authenticated
+
+    @property
+    def is_active(self):
+        """Always True, as all users are active."""
+        return True
+
+    @property
+    def is_anonymous(self):
+        """Always False, as anonymous users aren't supported."""
+        return False
+
+    def get_id(self):
+        """Return the email address to satisfy Flask-Login's requirements."""
+        """Requires use of Python 3"""
+        return str(self.id)
+
+    def __repr__(self):
+        return '<User {0}>'.format(self.name)
